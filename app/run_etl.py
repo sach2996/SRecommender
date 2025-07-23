464353f637d02830dc195727e59b86ded40560bc
Sachin Sharma <sach2996@gmail.com>
2025-07-23 15:54:43 -0400

    removing and starting a fresh app

--- app/run_etl.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from services.etl import run_recommendation_etl
-
-if __name__ == "__main__":
-    asyncio.run(run_recommendation_etl())
--- app/services/recommneders/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Protocol
-
-class BaseRecommender(ABC):
-    @abstractmethod
-    def recommend(self, prices: list[float]) -> bool:
-        """Return True if BUY signal, else False."""
-        pass
